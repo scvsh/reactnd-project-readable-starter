892a0b2b9cfffb792355ef8e40a125fad826fc60
Arseni Zemskow <scvsh@vetki.us>
2018-03-29 16:59:52 +0300

    Cleanup

--- scribbble/src/App.js
+++ scribbble/src/App.js
@@ -3,7 +3,6 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 import PostDetail from './components/PostDetail.js';
 import PostDashboard from './components/PostDashboard.js';
-import NotFound from './components/NotFound.js';
 
 class App extends Component {
     render() {
@@ -13,7 +12,6 @@ class App extends Component {
                     <Route exact path="/" component={PostDashboard} />
                     <Route exact path="/:category" component={PostDashboard} />
                     <Route exact path="/:category/:id" component={PostDetail} />
-                    <Route component={NotFound} />
                 </Switch>
             </div>
         );
